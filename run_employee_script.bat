39bd5dec03c3d3677d83f330c71e361cb18a95a4
narck25 <91917586+narck25@users.noreply.github.com>
2026-04-29 22:14:06 -0600

    chore: limpiar archivos de prueba y scripts innecesarios del repo

--- run_employee_script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Ejecutando script para crear registro de empleado...
-cd /d "c:\xampp\htdocs\Mini-ERP-Kram\backend"
-node create_employee_record.js
-pause
--- verify_employee.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Ejecutando verificación del empleado...
-cd /d "c:\xampp\htdocs\Mini-ERP-Kram\backend"
-node verify_employee.js
-pause
